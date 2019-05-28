bfc9eec0c8b250a4ab423b82fe882ee5c6d103d2
Eduardo Lemos Rocha <edu@localhost.localdomain>
2019-05-27 22:34:17 -0300

    Arquivo lcd.c refatorado e portado

--- lcd.c
+++ lcd.c
@@ -2,91 +2,71 @@
 #include <stdint.h>
 #include "i2c.c"
 
-#define BIT_BL BIT3
-#define BIT_E BIT2
-#define BIT_RW BIT1
-#define BIT_RS BIT0
-
-// Incializar LCD modo 4 bits
+// Initialize LCD in 4 bits mode
 void LCD_B2_inic(uint8_t addr){
 
-    PCF_STT_STP(addr);      //Colocar PCF em estado conhecido
+    B2_STT_STP(addr);                                    // Put LCD in a known state
 
-    // Preparar I2C para operar
-    UCB2I2CSA = addr;    //Endereço Escravo
-    UCB2CTLW0 |= UCTR    |   //Mestre TX
-                UCTXSTT;    //Gerar START
-    while ( (UCB2IFG & UCTXIFG0) == 0);          //Esperar TXIFG=1
-    UCB2TXBUF = 0;                              //Saída PCF = 0;
-    while ( (UCB2CTLW0 & UCTXSTT) == UCTXSTT);   //Esperar STT=0
-    if ( (UCB2IFG & UCNACKIFG) == UCNACKIFG)    //NACK?
-                while(1);
+    UCB2I2CSA = addr;                                    // Select slave's address
+    SetFlag(UCB2CTLW0, UCTR);                            // Set as transmitter
+    SetFlag(UCB2CTLW0, UCTXSTT);                         // Init Start
+    while(CompareFlagEQ(UCB2IFG, UCTXIFG0, 0));          // Wait TXIFG0 (with master at I2COA0)
+    UCB2TXBUF = 0;                                       // Slave's exit = 0;
+    while(CompareFlagEQ(UCB2CTLW0, UCTXSTT, UCTXSTT));   // Wait Start
+    if(CompareFlagEQ(UCB2IFG, UCTXIFG0, UCNACKIFG))      // NACK?
+        while(1);                                        // If NACK do nothing
 
-    // Começar inicialização
-    LCD_B2_aux(0);     //RS=RW=0, BL=1
+    // Start Initialization
+    LCD_B2_aux(0);                                       // RS = RW = 0, BL=1
     delay(20000);
-    LCD_B2_aux(3);     //3
+    LCD_B2_aux(3);                                       //3
     delay(10000);
-    LCD_B2_aux(3);     //3
+    LCD_B2_aux(3);                                       //3
     delay(10000);
-    LCD_B2_aux(3);     //3
+    LCD_B2_aux(3);                                       //3
     delay(10000);
-    LCD_B2_aux(2);     //2
+    LCD_B2_aux(2);                                       //2
 
-    // Entrou em modo 4 bits
-    LCD_B2_aux(2);     LCD_B2_aux(8);     //0x28
-    LCD_B2_aux(0);     LCD_B2_aux(8);     //0x08
-    LCD_B2_aux(0);     LCD_B2_aux(1);     //0x01
-    LCD_B2_aux(0);     LCD_B2_aux(6);     //0x06
-    LCD_B2_aux(0);     LCD_B2_aux(0xF);   //0x0F
+    // Enter 4 bits mode
+    LCD_B2_aux(2);     LCD_B2_aux(8);                    //0x28
+    LCD_B2_aux(0);     LCD_B2_aux(8);                    //0x08
+    LCD_B2_aux(0);     LCD_B2_aux(1);                    //0x01
+    LCD_B2_aux(0);     LCD_B2_aux(6);                    //0x06
+    LCD_B2_aux(0);     LCD_B2_aux(0xF);                  //0x0F
 
-    while ( (UCB2IFG & UCTXIFG0) == 0)   ;          //Esperar TXIFG=1
-    UCB2CTLW0 |= UCTXSTP;                           //Gerar STOP
-    while ( (UCB2CTLW0 & UCTXSTP) == UCTXSTP)   ;   //Esperar STOP
-    delay(50);
+    while(CompareFlagEQ(UCB2IFG, UCTXIFG0, 0));          // Wait TXIFG0 (with master at I2COA0)
+    SetFlag(UCB2CTLW0, UCTXSTP);                         // Call Stop
+    while(CompareFlagEQ(UCB2CTLW0, UCTXSTP, UCTXSTP));   // Wait Stop
+    delay(500);
 }
 
-// Auxiliar inicialização do LCD (RS=RW=0)
-// *** Só serve para a inicialização ***
-void LCD_B2_aux(char dado){
-    while ( (UCB2IFG & UCTXIFG0) == 0);              //Esperar TXIFG=1
-    UCB2TXBUF = ((dado<<4)&0XF0) | BIT_BL;          //PCF7:4 = dado;
+// Auxiliar for LCD's initialization (RS = RW = 0)
+// *** Only for initialization ***
+void LCD_B2_aux(char data){
+    while(CompareFlagEQ(UCB2IFG, UCTXIFG0, 0));          // Wait TXIFG0 (with master at I2COA0)
+    UCB2TXBUF = ((data<<4)&0XF0) | BIT_BL;               //PCF7:4 = dado;
     delay(50);
-    while ( (UCB2IFG & UCTXIFG0) == 0);              //Esperar TXIFG=1
-    UCB2TXBUF = ((dado<<4)&0XF0) | BIT_BL | BIT_E;  //E=1
+    while(CompareFlagEQ(UCB2IFG, UCTXIFG0, 0));          // Wait TXIFG0 (with master at I2COA0)
+    UCB2TXBUF = ((data<<4)&0XF0) | BIT_BL | BIT_E;       //E=1
     delay(50);
-    while ( (UCB2IFG & UCTXIFG0) == 0);              //Esperar TXIFG=1
-    UCB2TXBUF = ((dado<<4)&0XF0) | BIT_BL;          //E=0;
+    while(CompareFlagEQ(UCB2IFG, UCTXIFG0, 0));          // Wait TXIFG0 (with master at I2COA0)
+    UCB2TXBUF = ((data<<4)&0XF0) | BIT_BL;               //E=0;
 }
 
-void B2_write_LCD(char dado){
+// Function to write in the LCD with UCB2 (must config B2 first)
+void B2_write_LCD(char data){
 	UCB2I2CSA = 0x27;
-	UCB2CTLW0 |= UCTR |
-    			 UCTXSTT;
-    while ( (UCB2IFG & UCTXIFG0) == 0);      //espera TXIFG
-    if ( (UCB2IFG & UCTXIFG0) == UCNACKIFG)  //NACK?
-        while(1);
+    SetFlag(UCB2CTLW0, UCTR);                            // Set as transmitter
+    SetFlag(UCB2CTLW0, UCTXSTT);                         // Init Start
+    while(CompareFlagEQ(UCB2IFG, UCTXIFG0, 0));          // Wait TXIFG0 (with master at I2COA0)
+    if(CompareFlagEQ(UCB2IFG, UCTXIFG0, UCNACKIFG))      // NACK?
+        while(1);                                        // If NACK do nothing
     delay(500);
-    UCB2TXBUF = dado;
+    UCB2TXBUF = data;                                    // Data into transmission buffer
     delay(500);
-    while ( (UCB2IFG & UCTXIFG0) == 0);      //espera TX
+    while(CompareFlagEQ(UCB2IFG, UCTXIFG0, 0));          // Wait TXIFG0 (with master at I2COA0)
     delay(500);
-    UCB2CTLW0 |= UCTXSTP;    //gera STOP
-    while ( (UCB2CTLW0 & UCTXSTP) == UCTXSTP);
+    SetFlag(UCB2CTLW0, UCTXSTP);                         // Call Stop
+    while(CompareFlagEQ(UCB2CTLW0, UCTXSTP, UCTXSTP));   // Wait Stop
     delay(500);
 }
-
-// void B2_write_LCD(char dado){
-// 	UCB2I2CSA = 0x27;
-// 	UCB2CTLW0 |= UCTR |
-//     			 UCTXSTT;
-//     while ( (UCB2IFG & UCTXIFG0) == 0);      //espera TXIFG
-//     UCB2TXBUF = dado;
-//     while ( (UCB2CTLW0 & UCTXSTT) == UCTXSTT);
-//     if ( (UCB2IFG & UCTXIFG0) == UCNACKIFG)  //NACK?
-//         while(1);
-//     while ( (UCB2IFG & UCTXIFG0) == 0);      //espera TX
-//     UCB2CTLW0 |= UCTXSTP;    //gera STOP
-//     while ( (UCB2CTLW0 & UCTXSTP) == UCTXSTP);
-//     delay(500);
-// }
